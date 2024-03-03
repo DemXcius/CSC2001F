be78c7cd290c35547d39d9a82be17f3ce9e6cae2
skippy <schchr077@myuct.ac.za>
2024-03-03 21:09:02 +0200

    Method to get text file size is created

--- Assignment 1/GenericsKbArrayApp.java
+++ Assignment 1/GenericsKbArrayApp.java
@@ -1,7 +1,29 @@
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class GenericsKbArrayApp {
-    public static void main (String[] args){
+    private static int NumLines() {
+        int lines = 0;
+        String fileName = "GenericsKB-additional.txt";
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+    
+            while (bufferedReader.readLine() != null) {
+                lines++;
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.err.println("Error reading the file: " + e.getMessage());
+    
+        }
+        return lines;
+    }
+    public static void main(String args) {
+        int numLines = NumLines();
+        String[] arrFile = new String[numLines];
         
     }
 }
