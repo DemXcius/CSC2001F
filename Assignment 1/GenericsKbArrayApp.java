fc41fb3a0176c30b8e1eda1106e9641528ed002b
skippy <schchr077@myuct.ac.za>
2024-03-03 21:31:00 +0200

    Added scanner functionality

--- Assignment 1/GenericsKbArrayApp.java
+++ Assignment 1/GenericsKbArrayApp.java
@@ -23,10 +23,10 @@ public class GenericsKbArrayApp {
         return lines;
     }
 
-    public static void main(String args) {
+    public static String[] PopulateArray() {
+        // Code to get arrFile to contain all the lines from the file
         int numLines = NumLines();
         String[] arrFile = new String[numLines];
-        // Code to get arrFile to contain all the lines from the file
         try {
             FileReader fileReader = new FileReader(fileName);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -38,12 +38,26 @@ public class GenericsKbArrayApp {
             }
             bufferedReader.close();
 
-            
-
         } catch (IOException e) {
             System.err.println("Error reading the file: " + e.getMessage());
         }
-        
+        return arrFile;
+
+    }
+
+    public static void main(String args) {
+        String[] arrFile = PopulateArray();
+        String input = "";
+        Scanner keyboard = new Scanner(System.in);
+        input = keyboard.nextLine();
+        System.out.println("Choose an action from the menu:\r\n" + //
+                "1. Load a knowledge base from a file\r\n" + //
+                "2. Add a new statement to the knowledge base\r\n" + //
+                "3. Search for an item in the knowledge base by term\r\n" + //
+                "4. Search for a item in the knowledge base by term and sentence\r\n" + //
+                "5. Quit\r\n" + //
+                "Enter your choice");
+
     }
 
 }
