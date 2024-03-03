7ad548bb6273a68ae4844c59320b706ba3b91b51
skippy <schchr077@myuct.ac.za>
2024-03-03 21:15:52 +0200

    Array contains all lines from text file

--- Assignment 1/GenericsKbArrayApp.java
+++ Assignment 1/GenericsKbArrayApp.java
@@ -4,26 +4,46 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class GenericsKbArrayApp {
-    private static int NumLines() {
+    public static String fileName = "GenericsKB-additional.txt";
+
+    public static int NumLines() {
         int lines = 0;
-        String fileName = "GenericsKB-additional.txt";
         try {
             FileReader fileReader = new FileReader(fileName);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-    
+
             while (bufferedReader.readLine() != null) {
                 lines++;
             }
             bufferedReader.close();
         } catch (IOException e) {
             System.err.println("Error reading the file: " + e.getMessage());
-    
+
         }
         return lines;
     }
+
     public static void main(String args) {
         int numLines = NumLines();
         String[] arrFile = new String[numLines];
+        // Code to get arrFile to contain all the lines from the file
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line;
+            int index = 0;
+            while ((line = bufferedReader.readLine()) != null) {
+                arrFile[index++] = line;
+            }
+            bufferedReader.close();
+
+            
+
+        } catch (IOException e) {
+            System.err.println("Error reading the file: " + e.getMessage());
+        }
         
     }
+
 }
