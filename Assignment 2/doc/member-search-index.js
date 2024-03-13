1bffd95aef4b797d1464462dd39a9da1df5c8eac
skippy <schchr077@myuct.ac.za>
2024-03-13 11:47:47 +0200

    generated the Javadocs.

---
+++ Assignment 2/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"GenericsKbAVLApp","l":"GenericsKbAVLApp()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"GenericsKbAVLApp","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
